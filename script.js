4947fdfc53c41f162b8786616d9de53c0439c912
Natacha Tavares <natacha.alves@eaportal.org>
2025-04-26 17:17:59 -0300

    arrow function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //funções anônimas = funções sem nome
 
 //função anônima comum / antiga
-
+/*
 let mensagem = function() {
 console.log("Bem-vindo(a)!")
 }
@@ -12,3 +12,17 @@ return valor1 + valor2
 
 console.log(mensagem())
 console.log(somar(10, 20))
+*/
+
+//arrow function
+
+let mensagem = () => {
+    console.log("Olá, tudo bem?")
+}
+
+let somar = (valor1, valor2) => valor1 + valor2
+let raizQuadrada = (valor1) => Math.sqrt(valor1)
+
+console.log(mensagem())
+console.log(somar(25, 768))
+console.log(raizQuadrada(49))
